999e29c9b57f113daf8ec15708dc3a390a111f25
NQHUng2805 <hungnq2805@gmail.com>
2026-01-20 06:02:38 +0700

    camera

--- frontend/src/components/quiz/create.jsx
+++ frontend/src/components/quiz/create.jsx
@@ -5,6 +5,8 @@ import {
     required,
     ReferenceArrayInput,
     SelectArrayInput,
+    ReferenceInput,
+    SelectInput,
 } from "react-admin";
 
 const QuizCreate = () => {
@@ -12,9 +14,12 @@ const QuizCreate = () => {
         <div className="w-full min-h-[calc(100vh-72px)] py-14 px-10">
             <Create>
                 <SimpleForm>
-                    <TextInput source="title" validate={[required()]} label="Title" />
-                    <TextInput source="description" validate={required()} label="Description" />
-                    <ReferenceArrayInput source="question" reference="question" label="Select Questions">
+                    <TextInput source="title" validate={[required()]} label="Title" fullWidth />
+                    <ReferenceInput source="course" reference="course" label="Course">
+                        <SelectInput optionText="courseTitle" validate={[required()]} />
+                    </ReferenceInput>
+                    <TextInput source="description" validate={required()} label="Description" fullWidth multiline />
+                    <ReferenceArrayInput source="question" reference="question" label="Select Questions" fullWidth>
                         <SelectArrayInput optionText="question" />
                     </ReferenceArrayInput>
                 </SimpleForm>
