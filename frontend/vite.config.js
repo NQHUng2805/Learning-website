293e416e521378d5c37ee0d24885f3c0833ebf60
NQHUng2805 <hungnq2805@gmail.com>
2026-01-20 02:09:34 +0700

    remove hardcode url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', //Backend server
+        target: process.env.VITE_API_URL || 'http://localhost:5000', //Backend server
         changeOrigin: true,
         secure: false,
       },
